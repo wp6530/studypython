65044e9fffb44e491949585d0dc919355136686c
wp6530 <wp6530wp@outlook.com>
2019-10-28 12:10:08 +0800

    添加python字符串

---
+++ 01python基础/变量类型/字符串.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+# 字符串是 Python 中最常用的数据类型。我们可以使用引号( ' 或 " )来创建字符串。
+var1 = "Hello World"
+var2 = "wp6530"
+
+# 访问字符串中的值
+print ("var1[0]: ", var1[0])
+print ("var2[1:5]: ", var2[1:5])
+
+# 字符串更新
+print("已更新字符串：", var1[:6]+'wp6530')
+
+# 字符串运算符
+a = "hello"
+b = "python"
+
+print("a + b :", a+b)
+print("a * 2 :", a*2)
+# [] 左闭右开
+print("a[1] :", a[1])
+print("a[1:4] :", a[1:4])
+# 按照字符字面的意思使用，没有转义特殊或不能打印的字符
+print(r'\n')
+print(R'\n')
+
+# 字符串格式化
+
+# 字符串方法
+# 详细参考 https://www.runoob.com/python3/python3-string.html
